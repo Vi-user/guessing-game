3e550bbaa64c63edb02bebd03074ea616ca67675
Vi-user <vi.volkova.2712@gmail.com>
2022-09-12 14:29:11 +0300

    feat: task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max, match) {
+        this.min = 0;
+        this.max = 0;
+        this.match = 0;
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.match = Math.ceil((this.max - this.min)/2)+this.min
+        return this.match;
     }
 
     lower() {
-
+        return this.max = this.match;
     }
 
     greater() {
-
+        return this.min = this.match;
     }
 }
 
